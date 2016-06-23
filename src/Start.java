f2c171bff6f856e47e89c8295abbb57b7dc5f3f6
Piotr Jankowski <p.jankowski92@gmail.com>
2016-06-23 06:50:47 +0200

    Deleted uncecessary imports in main

--- src/Start.java
+++ src/Start.java
@@ -1,12 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-
-import algorithms.basic.Factorial;
-import algorithms.basic.Fibonacci;
-import algorithms.basic.FuzzBuzz;
-import algorithms.basic.StringAndChar;
 import algorithms.sort.BubbleSort;
 import algorithms.sort.InsertionSort;
 import algorithms.sort.MergeSort;
@@ -15,16 +9,11 @@ import algorithms.sort.SelectionSort;
 import algorithms.sort.ShellSort;
 import algorithms.sort.Sort;
 import algorithms.sort.SortTester;
-import dataStructures.ArrayQueue;
-import dataStructures.DynamicArray;
-import dataStructures.DynamicArrayStack;
-import dataStructures.LinkList;
-import dataStructures.LinkStack;
-
 
 public class Start {
 
 	public static void main(String[] args) {
+
 		// Sorting
 		System.out.println("----------Sorting----------");
 		Sort bubbleSort = new BubbleSort();
@@ -34,13 +23,10 @@ public class Start {
 		Sort mergeSort = new MergeSort();
 		Sort quickSort = new QuickSort();
 		List<Sort> sortList = new ArrayList<Sort>();
-		sortList.addAll(Arrays.asList(bubbleSort, insertionSort, selectionSort, shellSort, mergeSort,quickSort));
+		sortList.addAll(Arrays.asList(bubbleSort, insertionSort, selectionSort, shellSort, mergeSort, quickSort));
 		SortTester sortTester = new SortTester();
 		sortTester.executeSortAndPrint(sortList, 20);
 
-
-		
-		
 	}
 
 }
