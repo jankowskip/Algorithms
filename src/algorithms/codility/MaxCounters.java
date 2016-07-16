d581319b19583b1d6aeda4e7b813e824a7aa478d
Piotr Jankowski <p.jankowski92@gmail.com>
2016-07-16 02:04:08 +0200

    MaxCounters improvement

--- src/algorithms/codility/MaxCounters.java
+++ src/algorithms/codility/MaxCounters.java
@@ -1,11 +1,13 @@
 package algorithms.codility;
 
+public class MaxCounters { // Codility result: 88% corectness: 100% performance: 80%
+							
 
-public class MaxCounters { //Codility result: 77% corectness: 100% performance: 60%
-	
 	public int[] solution(int N, int[] A) {
 		int[] result = new int[N];
 		int maxValue = 0;
+		int numberToAdd = 0;
+		int lastNumberToAdd = 0;
 		for (int i = 0; i < A.length; i++) {
 			if (A[i] < N + 1) {
 				result[A[i] - 1]++;
@@ -13,13 +15,16 @@ public class MaxCounters { //Codility result: 77% corectness: 100% performance: 
 					maxValue = result[A[i] - 1];
 				}
 			} else {
-				for (int j = 0; j < N; j++) {
-					result[j] = maxValue;
-				}
+				lastNumberToAdd = numberToAdd;
+				numberToAdd = maxValue + lastNumberToAdd;
+				result = new int[N];
+				maxValue = 0;
 			}
+		}
+		for (int j = 0; j < N; j++) {
+			result[j] += numberToAdd;
 		}
 		return result;
 	}
-
 
 }
