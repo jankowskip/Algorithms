5c6f00a3a42f8b189e3bf0e10919f161fdba2777
Piotr Jankowski <p.jankowski92@gmail.com>
2016-07-16 03:00:59 +0200

    MaxCounters improvement

--- src/Start.java
+++ src/Start.java
@@ -29,15 +29,6 @@ public class Start {
 		sortList.addAll(Arrays.asList(bubbleSort, insertionSort, selectionSort, shellSort, mergeSort, quickSort));
 		SortTester sortTester = new SortTester();
 		sortTester.executeSortAndPrint(sortList, 60);
-		
-		int N = 5;
-//		int[] A = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,1,2,3,4,5,6,7,8,9,10,11,12,13,14,1,15,15,15,15,15,15,15,15,1,15};
-		
-		int[] A = {1,2,3,4,5,6,6,6,1,6};
-		MaxCounters mc = new MaxCounters();
-		mc.solution(N, A);
-//		(1,[1,1,1,2])
 
 	}
-
 }
--- src/algorithms/codility/MaxCounters.java
+++ src/algorithms/codility/MaxCounters.java
@@ -1,28 +1,30 @@
 package algorithms.codility;
 
-public class MaxCounters { // Codility result: 88% corectness: 100% performance: 80%
+public class MaxCounters { // Codility result: 100%
 							
 
 	public int[] solution(int N, int[] A) {
 		int[] result = new int[N];
 		int maxValue = 0;
-		int numberToAdd = 0;
-		int lastNumberToAdd = 0;
+		int minValue = 0;
 		for (int i = 0; i < A.length; i++) {
 			if (A[i] < N + 1) {
-				result[A[i] - 1]++;
+				if (result[A[i] - 1] >= minValue) {
+					result[A[i] - 1]++;
+				} else {
+					result[A[i] - 1] = minValue + 1;
+				}
 				if (result[A[i] - 1] > maxValue) {
 					maxValue = result[A[i] - 1];
 				}
 			} else {
-				lastNumberToAdd = numberToAdd;
-				numberToAdd = maxValue + lastNumberToAdd;
-				result = new int[N];
-				maxValue = 0;
+				minValue = maxValue;
 			}
 		}
 		for (int j = 0; j < N; j++) {
-			result[j] += numberToAdd;
+			if (result[j] < minValue) {
+				result[j] = minValue;
+			}
 		}
 		return result;
 	}
