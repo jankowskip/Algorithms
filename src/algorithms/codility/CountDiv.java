3bddacf1cb4dc73f2971b85a4f6f141067a42794
Piotr Jankowski <p.jankowski92@gmail.com>
2016-07-16 05:50:43 +0200

    Codility CountDiv solution

--- src/algorithms/codility/CountDiv.java
+++ src/algorithms/codility/CountDiv.java
@@ -1,5 +1,10 @@
 package algorithms.codility;
 
 public class CountDiv {
-
+	 public int solution(int A, int B, int K) {
+	       int result =B/K;
+	       int numbersToA = (A-1)/K;
+	       result = result - numbersToA;
+	       return result;
+	    }
 }
